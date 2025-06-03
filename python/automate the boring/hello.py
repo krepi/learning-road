d36225c2efa7675b9dfeb71fc8489e16b3c1047c
Przemyslaw Krepski <krepski80@gmail.com>
2025-06-03 17:58:04 +0200

    Initial folder structure

---
+++ python/automate the boring/hello.py
@@ -0,0 +1,10 @@
+# this programssays hello and name
+print('hello world')
+print('what is your name?')
+myName = input()
+print('it is good to meet you' + myName)
+print('the length of your name is:')
+print(len(myName))
+print('what is your age?')
+myAge= input()
+print('you will be ' + str(int(myAge) +1) + ' in one year')
