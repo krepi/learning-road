03612b9d69c50878f296ffd44418a3ab3002e923
Przemyslaw Krepski <krepski80@gmail.com>
2025-06-25 21:30:46 +0200

    calculator

---
+++ python/100 days/day1/thonny_day_10.py
@@ -0,0 +1,12 @@
+def format_name(first_name, last_name):
+    return f"{first_name.title()} {last_name.title()}"
+
+print(format_name("przemek", "krepski"))
+
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+
+print(format_name("AnGEla", "YU"))
