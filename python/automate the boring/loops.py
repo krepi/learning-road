afb452b1a4d59a93da02b07c37b255d632acd8ad
Przemyslaw Krepski <krepski80@gmail.com>
2025-06-26 21:20:11 +0200

    udemy malfunction, loops

---
+++ python/automate the boring/loops.py
@@ -0,0 +1,16 @@
+spam = 0
+while spam < 5:
+    print("hello world")
+    spam += 1
+name = ''
+while name !="your name":
+    print("please type your name")
+    name = input()
+print("thx")
+name = ''
+while True:
+    print("please type your name")
+    name = input()
+    if name == "your name":
+        break
+print("thx")
